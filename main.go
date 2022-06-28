8639da5e4222ef2ada456a99a6ec9e8c7d27caf7
Ali Aydin <aliaydin06@gmail.com>
2022-06-27 22:49:01 -0400

    array, slice, map

--- main.go
+++ main.go
@@ -18,15 +18,32 @@ func main() {
 	//define variables
 	var count int
 	var age int = 25
-	pi := 3.14
+	pi := 3.14 //without specify type, compiler will do it for us
 
 	fmt.Println(count, age, pi)
 
-	// contiditon
+	// if contiditon
 	if age >= 30 {
 		fmt.Println("You are old")
 	} else if age < 30 {
 		fmt.Println("You are young")
 	}
+
+	// array / slice
+	numbers := [5]int{4, 7, 1, 5, 2}                  // fixed size array
+	strings := []string{"aa", "bb", "cc", "dd", "ee"} // a slice
+
+	strings = append(strings, "ff") // you can modify later
+
+	fmt.Println(numbers)
+	fmt.Printf("%q\n", strings)
+
+	// map
+	phones := make(map[string]int) // map[<key type>]<value type>
+
+	phones["dave"] = 2012348866
+	phones["katy"] = 2014327499
+
+	fmt.Println(phones)
 
 }
